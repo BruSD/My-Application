0a51a5058560c34921526b6813148580da89100e
BruSD <brusd.com@gmail.com>
2014-10-14 22:02:38 +0300

    Рессивер

--- app/src/main/java/test/test/myapplication/FragmentActivityDualOrientation.java
+++ app/src/main/java/test/test/myapplication/FragmentActivityDualOrientation.java
@@ -3,18 +3,25 @@ package test.test.myapplication;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import test.test.myapplication.fragments.DeteilsFragment;
 import test.test.myapplication.fragments.ListOnFragment;
 
 
 public class FragmentActivityDualOrientation extends Activity {
-
+    private FragmentMessBroadcastReceiver fragmentMessBroadcastReceiver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_activity_dual_orientation);
+
+        fragmentMessBroadcastReceiver =  new FragmentMessBroadcastReceiver();
 
         int orient = this.getResources().getConfiguration().orientation;
         if (orient == 1) {
@@ -33,5 +40,26 @@ public class FragmentActivityDualOrientation extends Activity {
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("SendMess111");
+        registerReceiver(fragmentMessBroadcastReceiver, filter);
+    }
 
+    class FragmentMessBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals("SendMess111")) {
+                String mess = intent.getStringExtra("messsss");
+
+                Toast.makeText(context, "" + mess, Toast.LENGTH_LONG).show();
+
+
+
+            }
+        }
+    }
 }
--- app/src/main/java/test/test/myapplication/fragments/ListOnFragment.java
+++ app/src/main/java/test/test/myapplication/fragments/ListOnFragment.java
@@ -1,10 +1,14 @@
 package test.test.myapplication.fragments;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import test.test.myapplication.R;
 
@@ -17,6 +21,15 @@ public class ListOnFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_with_list_layout, container, false);
+        TextView sendBroadTextView = (TextView)rootView.findViewById(R.id.send_broad_text_view);
+        sendBroadTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent("SendMess111");
+                intent.putExtra("messsss", "Поле было кликнуто!");
+                getActivity().sendBroadcast(intent);
+            }
+        });
         return rootView;
     }
 }
